51adc70cb80fa832a679b7bcb2ab8bbabb6f9171
zainafzal-tek <zainafzal@gmail.com>
2020-11-10 23:13:49 -0500

    added Important util files

--- src/test/java/com/zerobank/utilities/ConfigurationReader.java
+++ src/test/java/com/zerobank/utilities/ConfigurationReader.java
@@ -1,4 +1,31 @@
 package com.zerobank.utilities;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 public class ConfigurationReader {
+
+    private static Properties properties = new Properties();
+
+    static {
+
+        try {
+            FileInputStream file = new FileInputStream("configuration.properties");
+
+            properties.load(file);
+
+            file.close();
+
+        } catch (IOException e){
+            System.err.println("Properties file could not be located!");
+        }
+
+    }
+
+    public static String getProperty(String keyword){
+        return properties.getProperty(keyword);
+    }
+
+
 }
---
+++ src/test/java/com/zerobank/utilities/GetSelectDropDowns.java
@@ -0,0 +1,10 @@
+package com.zerobank.utilities;
+
+import org.openqa.selenium.support.ui.Select;
+
+public interface GetSelectDropDowns {
+
+    Select getSelectDropDown(String dropDownName);
+
+
+}
