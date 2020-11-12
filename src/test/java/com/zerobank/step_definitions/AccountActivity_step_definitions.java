538ddc340e9f99925b5b76653b17330ece230a97
evgeniya133 <euu1992@gmail.com>
2020-11-12 16:01:31 -0500

    updates on AccountActivity_step_definitions

--- src/test/java/com/zerobank/step_definitions/AccountActivity_step_definitions.java
+++ src/test/java/com/zerobank/step_definitions/AccountActivity_step_definitions.java
@@ -14,6 +14,11 @@ public class AccountActivity_step_definitions {
             accountActivityPage.navigateTo(string);
     }
 
+    @Then("user should see {string} page displayed")
+    public void user_should_see_page_displayed(String string) {
+        accountActivityPage.verifyTitle(string);
+    }
+
     @Then("transactions table should have column names {string}, {string}, {string}, {string}")
     public void transactions_table_should_have_column_names(String string, String string2, String string3, String string4) {
             accountActivityPage.verifyHeaders(string, string2, string3, string4);
